1f7e9ff5cab6c06ae17c7c22b6d7cb182f8d7007
msh9637@g.rit.edu <msh9637@g.rit.edu@b84e0eb4-9d39-9fcf-da1e-ede19924a2a6>
2013-09-07 18:13:17 +0000

    Improvements to thread management on application shutdown.

--- apps/MousePositionRecorder/src/mousepositionrecorder/MousePositionRecorder.java
+++ apps/MousePositionRecorder/src/mousepositionrecorder/MousePositionRecorder.java
@@ -3,43 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mousepositionrecorder;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author mhazlewood
  */
-public class MousePositionRecorder extends Application 
+public class MousePositionRecorder extends Application
 {
-    @Override
-    public void start(Stage stage) throws Exception 
-    {
-       stage.setResizable(false);
-        Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
+   private MainLayoutController mMainController;
+   
+   @Override
+   public void start(Stage stage) throws Exception
+   {
+      stage.setResizable(false);
+      
+      // Doing it this way so we can get access to the main controller class,
+      // so it can have knowledge of the application exit
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("MainLayout.fxml"));      
+      Parent root = (Parent)(loader.load());
+      mMainController = (MainLayoutController)(loader.getController());
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX 
-     * application. main() serves only as fallback in case the 
-     * application can not be launched through deployment artifacts,
-     * e.g., in IDEs with limited FX support. NetBeans ignores main().
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        launch(args);
-    }
+      Scene scene = new Scene(root);
+
+      stage.setScene(scene);
+      stage.show();
+      
+      // Add an event handler for the scene's close request, so we can tell the
+      // main layout controller to shut down properly
+      scene.getWindow().setOnCloseRequest(new EventHandler<WindowEvent>()
+      {
+         @Override
+         public void handle(WindowEvent t)
+         {
+            mMainController.shutdown();
+         }
+      });
+   }
+
+   /**
+    * The main() method is ignored in correctly deployed JavaFX application.
+    * main() serves only as fallback in case the application can not be launched
+    * through deployment artifacts, e.g., in IDEs with limited FX support.
+    * NetBeans ignores main().
+    *
+    * @param args the command line arguments
+    */
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
 
 }
